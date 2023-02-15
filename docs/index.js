51f9e17be69bdd055c93f04aca43fa65500749a2
Uyansu <suat1593@web.de>
2023-02-15 19:15:28 +0100

    aktuelle version

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,6 @@
 const form = document.querySelector(".addTodoItem");
-const input = document.querySelector(".newTaskInput");
-const listEl = document.querySelector(".toDoItem");
+const input = document.querySelector("#newTaskInput");
+const toDoList = document.querySelector(".listToDoItems");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -9,34 +9,18 @@ form.addEventListener("submit", (e) => {
 
     const taskEl = document.createElement("div");
     taskEl.classList.add("toDoItem");
-
-    const taskContentEl = document.createElement("label");
-    taskContentEl.classList.add("labelArea");
-
-    taskEl.appendChild(taskContentEl);
-
-    const taskInputEl = document.createElement("input");
-    taskInputEl.type = "checkbox";
-
-    taskContentEl.appendChild(taskInputEl);
-
-    const taskInputEl2 = document.createElement("input");
-    taskInputEl2.type = "text";
-
-    taskContentEl.appendChild(taskInputEl2);
     
-    const taskActionEl = document.createElement("div");
-    taskActionEl.classList.add(toDoItemMenu);
 
-    const editElement = document.createElement("img");
-    editElement.classList.add("edit");
-    editElement.src = "images/pencil.svg";
-
-    const deleteElement = document.createElement("img");
-    deleteElement.classList.add("delete");
-    deleteElement.src = "images/trash.svg";
-
-    taskActionEl.appendChild(editElement);
-    taskActionEl.appendChild(deleteElement);
-
+    taskEl.innerHTML = `
+    <label>
+        <input type="checkbox">
+        <span class="task">${task}</span>
+    </label>
+    <div class="toDoItemMenu">
+<img class="edit" src="images/pencil.svg">
+<img class="delete" src="images/trash.svg">
+</div>`;
+console.log(toDoList);
+toDoList.appendChild(taskEl);
 });
+
